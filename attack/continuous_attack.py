5285f8f2b1d7b0f56ff7f1bc7bf5ebe75a0a34c6
CJ Hosea <siagejustin@gmail.com>
2025-06-17 20:15:33 -0700

    Update continuous_attack.py

--- attack/continuous_attack.py
+++ attack/continuous_attack.py
@@ -8,7 +8,7 @@ from scapy.layers.inet import IP, TCP # packet layer access
 
 # path to pcap with modbus command
 # captured using: sudo tshark -i en0 -f "tcp port 502" -c 10 -w file_name
-pcap_path = "/Users/cj/Desktop/modbus_lab/cps-modbus-lab/cps-modbus-lab/pcap/one_write_coil_full.pcap"
+pcap_path = packets = rdpcap("/path/to/pcap")
 
 # try to read pcap
 try:
